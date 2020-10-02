b1b177c549773e4d5671762633498289d232da2f
Ludo <ludovic.eugenot@hotmail.fr>
2020-10-02 22:08:03 +0200

    Avancée Matrice

---
+++ Ctrl Alt/Assets/_Scripts/Map placement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+[CustomEditor(typeof(RectTransform))]
+public class Mapplacement : EditorWindow
+{
+    public RectTransform parent;
+    public RectTransform[,] itemsToArrange;
+
+    GUILayoutOption[] layoutOptions;
+
+    [MenuItem("Window/Arrange dots")]
+    public static void ShowWindow()
+    {
+        GetWindow<Mapplacement>("Map Placement Window");
+    }
+
+    public void OnGUI()
+    {
+        parent = (RectTransform)EditorGUILayout.ObjectField("Grid Parent", parent, typeof(RectTransform), true, layoutOptions);
+        if (GUILayout.Button("Arrange all dots in the map grid"))
+        {
+            ArrangeStuff();
+        }
+    }
+
+    public void ArrangeStuff()
+    {
+        itemsToArrange = new RectTransform[parent.childCount, parent.GetChild(0).childCount];
+
+        //for chaque enfant, on le bouge selon le nombre d'enfants par rapport à la taille de la grille ou plutôt par rapport aux ancres
+    }
+}
