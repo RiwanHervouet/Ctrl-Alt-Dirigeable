7c79d627fc0aa9974e4690ee87a80393b8ed32c3
Ludo <ludovic.eugenot@hotmail.fr>
2020-10-02 12:00:01 +0200

    Je ramène mon projet

---
+++ Ctrl Alt/Assets/_Scripts/FocusTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using Tobii.Gaming;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+
+public class FocusTest : MonoBehaviour
+{
+    #region Initiatlization
+    public GazeAware gaze;
+    private Button button;
+    #endregion
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        gaze = GetComponent<GazeAware>();
+        button = GetComponent<Button>();
+
+    }
+
+    void Update()
+    {
+        if (gaze.HasGazeFocus)
+        {
+            //button.Select
+        }
+    }
+}
