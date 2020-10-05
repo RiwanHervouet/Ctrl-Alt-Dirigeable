69e308199a01c4ae02fd5d2958a5d3848b942fae
Ludo <ludovic.eugenot@hotmail.fr>
2020-10-05 20:41:59 +0200

    Passage en Unity 2020.1.7 + avancée matrice

--- Ctrl Alt/Assets/_Scripts/Map placement.cs
+++ Ctrl Alt/Assets/_Scripts/Map placement.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEditor;
-using System.Linq;
+using UnityEngine.UI;
 
 [CustomEditor(typeof(RectTransform))]
 public class Mapplacement : EditorWindow
 {
-    public RectTransform parent;
-    public RectTransform[,] itemsToArrange;
+    private RectTransform parent;
+    private Rect gridData;
+    private RectTransform[,] itemsToArrange;
+    private Image imageToArrange;
 
     GUILayoutOption[] layoutOptions;
 
@@ -26,10 +28,26 @@ public class Mapplacement : EditorWindow
         }
     }
 
-    public void ArrangeStuff()
+    private void ArrangeStuff()
     {
-        itemsToArrange = new RectTransform[parent.childCount, parent.GetChild(0).childCount];
+        int numberOfVerticalItems = parent.childCount;
+        int numberOfHorizontalItems = parent.GetChild(0).childCount;
+        gridData = RectTransformUtility.PixelAdjustRect(parent, parent.parent.GetComponent<Canvas>());
+        itemsToArrange = new RectTransform[numberOfVerticalItems, numberOfHorizontalItems];
+        imageToArrange = parent.GetComponentInChildren<Image>() == null ? parent.GetChild(0).GetComponentInChildren<Image>() : parent.GetComponentInChildren<Image>();
+
+        float xSizeOfImageInGrid = gridData.width / (imageToArrange.preferredWidth * numberOfHorizontalItems);
+        float ySizeOfImageInGrid = gridData.height / (imageToArrange.preferredHeight * numberOfVerticalItems);
+
+        float sizeOfImageInGrid = xSizeOfImageInGrid <= ySizeOfImageInGrid ? xSizeOfImageInGrid : ySizeOfImageInGrid;
+
+        Debug.Log(RectTransformUtility.PixelAdjustRect(parent, parent.parent.GetComponent<Canvas>()));
 
         //for chaque enfant, on le bouge selon le nombre d'enfants par rapport à la taille de la grille ou plutôt par rapport aux ancres
+    }
+
+    private void DetermineSizes()
+    {
+        //mettre tout le début du code en initialisant bien comme il faut
     }
 }
