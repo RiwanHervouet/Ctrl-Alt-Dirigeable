45413b0430726c97949878f4b972d2f1a3c5d1d9
Ludo <ludovic.eugenot@hotmail.fr>
2020-10-08 19:54:03 +0200

    Navigation Start

---
+++ Ctrl Alt/Assets/_Scripts/Game Related/Modules/Module_Map.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+//MODULES ont les inputs et outputs alors que OutOfExperience sont le système
+public class Module_Map : MonoBehaviour
+{
+    #region Initiatlization
+	
+    #endregion
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Ctrl Alt/Assets/_Scripts/Game Related/Out Of Experience/OOE_Map.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OOE_Map : MonoBehaviour
+{
+    #region Initiatlization
+    #region 
+    public GameObject player;
+    #endregion
+
+    #region Map Characteristics to become Scriptable Object après les LD elements
+    [Header("Map Characteristics")]
+    [Tooltip("En case par seconde")]
+    [Range(0f, 3f)] public float speed = 1f;
+    [Tooltip("La matrice fait 32x32")]
+    public int[,] boundariesMap = new int[64, 32];
+    #endregion
+
+    #region LD elements to become Scriptable Object
+    [Header("LD elements")]
+    public GameObject spawnPlayer;
+    public GameObject mountain;
+    public GameObject storm;
+    public GameObject missile; // ou sinon des éclairs (ronds qui se rapprochent jusqu'à frapper une zone fortement quand ils sont tous sur le même point (la déflagration))
+    #endregion
+    #endregion
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
