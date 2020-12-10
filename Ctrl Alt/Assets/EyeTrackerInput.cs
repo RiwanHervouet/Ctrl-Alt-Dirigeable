36c03023535218e208e9260a51843f79c2d0e539
Ludo <ludovic.eugenot@hotmail.fr>
2020-12-10 19:24:13 +0100

    Début ajout eye tracker (push pour taffer chez oim)

---
+++ Ctrl Alt/Assets/EyeTrackerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EyeTrackerInput : MonoBehaviour
+{
+    #region Initialization
+    
+    #endregion
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
