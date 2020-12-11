3c0c58595086e658009140500ec038682e4df3be
RiwanH <r.hervouet@rubika-edu.com>
2020-12-11 21:46:41 +0100

    Dernier push

--- Ctrl Alt/Assets/_Scripts/Arduino Scripts/BlinkLED.cs
+++ Ctrl Alt/Assets/_Scripts/Arduino Scripts/BlinkLED.cs
@@ -6,28 +6,25 @@ using Uduino;
 public class BlinkLED : MonoBehaviour
 {
 
-    public int pinNumber; //numéro du pin de la LED
 
     void Start()
     {
 
-        UduinoManager.Instance.pinMode(pinNumber, PinMode.Output); //fait en sorte que la Pin soit un Output et pas un Input
-
     }
 
-    IEnumerator TurnOnLED () //Coroutine pour allumer une LED
+    public void TurnOnLED (pinNumber) //Coroutine pour allumer une LED
     {
-        
+
+        UduinoManager.Instance.pinMode(pinNumber, PinMode.Output);
+
         UduinoManager.Instance.digitalWrite(pinNumber, State.HIGH);
-        yield return new WaitForSeconds(0.1f);
 
     }
 
-    IEnumerator TurnOffLED() //Coroutine pour éteindre une LED
+    public void TurnOffLED() //Coroutine pour éteindre une LED
     {
 
         UduinoManager.Instance.digitalWrite(pinNumber, State.LOW);
-        yield return new WaitForSeconds(0.1f);
 
     }
 }
--- Ctrl Alt/Assets/_Scripts/Arduino Scripts/ReadSouffleur.cs
+++ Ctrl Alt/Assets/_Scripts/Arduino Scripts/ReadSouffleur.cs
@@ -1,19 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Uduino;
 
 public class ReadSouffleur : MonoBehaviour
 {
+
+    //1- Variables
+    public int soufflePin;
+    public int aspirationPin;
+    public bool souffle = false;
+    public bool aspiration = false;
     
+    //2- Initialisation des pins
     void Start()
     {
-        
+
+        UduinoManager.Instance.pinMode(soufflePin, PinMode.Input);
+        UduinoManager.Instance.pinMode(aspirationPin, PinMode.Input);
+
     }
 
-    
+    //3- Varification de l'état du souffleur
     void Update()
     {
-        
+
+        if (UduinoManager.Instance.digitalRead(soufflePin) > 0)
+        {
+
+            souffle = true;
+
+        }else if (UduinoManager.Instance.digitalRead(aspirationPin) > 0)
+        {
+
+            aspiration = true;
+
+        }else if (UduinoManager.Instance.digitalRead(soufflePin) <= 0)
+        {
+
+            souffle = false;
+
+        }
+        else if (UduinoManager.Instance.digitalRead(aspirationPin) <= 0)
+        {
+
+            aspiration = false;
+
+        }
+
     }
 
 }
